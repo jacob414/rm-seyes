ebe52e429021774fe066a70e7f235bed4083cd53
Jacob Oscarson <jacob@414soft.com>
2024-12-18 15:33:46 +0100

    Threw in a little config hook, just for myself ;-)

--- seyes-deploy.sh
+++ seyes-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set +x
+[ -f ~/.config/remarkablerc.sh ] && source ~/.config/remarkablerc.sh
 scp seyes.png $RM_HOST:/usr/share/remarkable/templates
 scp seyes.svg $RM_HOST:/usr/share/remarkable/templates
 ssh $RM_HOST systemctl restart xochitl
